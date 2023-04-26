e71945212b6cb8d615da7a098de607491bcb3bfd
Jazmine Miller <jazminedmiller@gmail.com>
2023-04-26 15:10:51 -0400

    add animation, tweak services box styling/spacing

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
+// let masks = document.querySelectorAll('.hero-lg-image')
+
+gsap.to(".hero-lg-image img", {
+    yPercent: 100,
+    duration: 1,
+    delay: 0.5, 
+  });
